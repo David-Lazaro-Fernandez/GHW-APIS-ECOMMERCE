e2ba776216ea4f142592228af64056d1e6064d21
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-04-10 05:52:40 +0000

    Models, sequelize, migrations and user controller created

---
+++ controllers/user-controller.js
@@ -0,0 +1,125 @@
+import db from '../models';
+
+const User = db.users;
+
+class UserController {
+    /*
+        C REATE USERS
+        R EAD ONE OR ALL THE USERS OF THE DB
+
+        U PDATE AN USER RECORD
+        D ELETE AN USER BY ITS ID
+    */
+
+    /**
+     *  Create a new user in the database.
+     * 
+     * @param {import('express').Request} req -The HTTP Request.
+     * @param {import('express').Response} res - The HTTP Response.
+     * @returns {Promise<void>}
+    */
+    async createUser(req, res) {
+        try {
+            // Create a User
+            const user = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
+            };
+            // Save User in the database
+            const data = await User.create(user);
+            res.status(201).send(data);
+        }
+        catch (error) {
+            res.status(500).send({
+                message: error.message || "Some error occurred while creating the User."
+            });
+        }
+    }
+
+    /**
+     * Fetches all the users from the users table and returns them as a JSON object.
+     *
+     * @param {import('express').Request} req - The HTTP Request. 
+     * @param {import('express').Request} res - The HTTP Response.
+     * @returns {Promise<void>}
+     */
+
+    async readUsers(req, res) {
+        try {
+            const data = await User.findAll();
+            res.status(200).send(data);
+        }
+        catch (error) {
+            res.status(500).send({
+                message: error.message || "Some error occurred while retrieving users."
+            });
+        }
+    }
+
+
+    /**
+     * Update an user info by the id in the request body.
+     *
+     * @param {import('express').Request} req - The HTTP Request. 
+     * @param {import('express').Request} res - The HTTP Response.
+     * @returns {Promise<void>}
+     */
+
+    async updateUser(req, res) {
+        const uid = req.params.id;
+        try {
+            const data = await User.update(req.body, {
+                where: { id: uid }
+            });
+            if (data == 1) {
+                res.status(204).send({
+                    message: "User was updated successfully."
+                });
+            } else {
+                res.status(404).send({
+                    message: `Cannot update User with id=${uid}. Maybe User was not found or req.body is empty!`
+                });
+            }
+        }
+        catch (error) {
+            res.status(500).send({
+                message: error.message || `Error updating User with id=${uid}`
+            });
+        } 
+    }
+
+    /**
+     * Deletes an user from the database by the id of the user.
+     *
+     * @param {import('express').Request} req - The HTTP Request. 
+     * @param {import('express').Request} res - The HTTP Response.
+     * @returns {Promise<void>}
+     */
+
+    async deleteUser(req, res) {
+        const uid = req.params.id;
+        try {
+            const data = await User.destroy({
+                where: { id: uid }
+            });
+            if (data == 1) {
+                res.status(204).send({
+                    message: "User was deleted successfully!"
+                });
+            } else {
+                res.status(404).send({
+                    message: `Cannot delete User with id=${uid}. Maybe User was not found!`
+                });
+            }
+        }
+        catch (error) {
+            res.status(500).send({
+                message: error.message || `Could not delete User with id=${uid}`
+            });
+        }
+
+    }
+}
+
+export default UserController;
--- models/client.ts
+++
@@ -1,6 +0,0 @@
-export interface Client {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-}
